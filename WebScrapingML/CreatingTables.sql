4a2a876146119dc276e6568b3816331bafebb84d
ambatista1982 <ambatista1982@gmail.com>
2023-02-27 14:35:36 -0800

    Add files via upload

---
+++ WebScrapingML/CreatingTables.sql
@@ -0,0 +1,53 @@
+use MLS_Project;
+DROP TABLE IF EXISTS TableRankBase;
+CREATE TABLE dbo.TableRankBase (
+    Generalkey int IDENTITY(1,1) PRIMARY KEY,
+	Rk INT NOT NULL, 
+	TEAM_NAME varchar(50) NOT NULL, 
+	MP INT, 
+	W INT, 
+	D INT, 
+	L INT, 
+	GF INT, 
+	GA INT, 
+	GD INT, 
+	PTS INT, 
+	PTS_MP FLOAT,
+	XG FLOAT, 
+	XGA FLOAT, 
+	XGD FLOAT, 
+	XGD_90 FLOAT, 
+	NOTES varchar(60),
+	CONFERENCE varchar(50),
+	SEASON INT NOT NULL
+);
+
+USE MLS_Project;
+DROP TABLE IF EXISTS TableDetailsBase;
+CREATE TABLE dbo.TableDetailsBase (
+	Generalkey INT IDENTITY(1,1) PRIMARY KEY,
+	TEAM_NAME varchar(50) NOT NULL, 
+	GAME_Date DATE NOT NULL,
+	GAME_Time TIME NOT NULL,
+	Comp varchar(50),
+	SEASON_Round varchar(100),
+	WEEK_DAY varchar(20),
+	TEAM_Venue varchar(20),
+	GAME_Result varchar(5),
+	GF INT,
+	GA INT,
+	Opponent varchar(50),
+	xG FLOAT,
+	xGA FLOAT,
+	Poss INT,
+	Attendance FLOAT,
+	Captain varchar(50),
+	Formation varchar(50),
+	Referee varchar(50),
+	Notes varchar(50),
+	Season INT
+);
+
+
+
+
